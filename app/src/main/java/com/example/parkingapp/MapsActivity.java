1f73edcf9a4932c2bbb68cc3fb416e409be0d3fa
CRISTIAN <cristiand.ruizr@utadeo.edu.co>
2019-09-30 23:41:10 -0500

    Se agregan marcadores en el mapa y se cambia localizacion para que apresca en la Universidad TAdeo

--- app/src/main/java/com/example/parkingapp/MapsActivity.java
+++ app/src/main/java/com/example/parkingapp/MapsActivity.java
@@ -8,6 +8,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -25,23 +27,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Utadeo = new LatLng(4.606698, -74.067626);
+        mMap.addMarker(new MarkerOptions().position(Utadeo).title("Universidad Jorge Tadeo Lozano").snippet("Descripcion").icon(BitmapDescriptorFactory.fromResource(R.drawable.parkingv1)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Utadeo,17));
+
+        LatLng ParqueaderoUtadeo = new LatLng(4.605704, -74.067262);
+        mMap.addMarker(new MarkerOptions().position(ParqueaderoUtadeo).title("Parqueadero la Tadeo").snippet("Descripcion 2").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(ParqueaderoUtadeo));
     }
 }
