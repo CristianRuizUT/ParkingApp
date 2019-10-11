2d50cfdc0aae681e7a1115aac92406ce694fe7c7
CRISTIAN <cristiand.ruizr@utadeo.edu.co>
2019-10-11 02:23:04 -0500

    Ajustes de popup al seleccionar un marcador en el mapa

--- app/src/main/java/com/example/parkingapp/MapsActivity.java
+++ app/src/main/java/com/example/parkingapp/MapsActivity.java
@@ -24,7 +24,7 @@ import java.util.Locale;
 public class MapsActivity extends AppCompatActivity implements GoogleMap.OnInfoWindowClickListener,GoogleMap.OnMarkerDragListener,OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
-    private Marker markerPrueba,markerDrag,InfoWindow;
+    private Marker markerTadeo,markerLaTadeo,markerPrueba,markerDrag,InfoWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,40 +40,46 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnInfoW
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // mMap.setMyLocationEnabled(true);
+
         //Punto U tadeo
         LatLng Utadeo = new LatLng(4.606698, -74.067626);
-        mMap.addMarker(new MarkerOptions()
+        markerTadeo = googleMap.addMarker(new MarkerOptions()
                 .position(Utadeo)
                 .title("Universidad Jorge Tadeo Lozano")
-                .snippet("Descripcion")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.parkingv1)));
+                .snippet("Ver mas!!!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
         //Punto parqueadro la tadeo
         LatLng ParqueaderoUtadeo = new LatLng(4.605704, -74.067262);
-        mMap.addMarker(new MarkerOptions()
+        markerLaTadeo = googleMap.addMarker(new MarkerOptions()
                 .position(ParqueaderoUtadeo)
                 .title("Parqueadero la Tadeo")
-                .snippet("Descripcion 2")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+                .snippet("Ver mas!!!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
         //Marker para dar clicl encima
         LatLng UtadeoBiblioteca = new LatLng(4.606503, -74.067066);
         markerPrueba = googleMap.addMarker(new MarkerOptions()
                 .position(UtadeoBiblioteca)
-                .title("Biblioteca utadeo"));
+                .title("Biblioteca utadeo")
+                .snippet("Ver mas!!!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
         //Marker para arrastrar
         LatLng PosicionMover = new LatLng(4.607607, -74.068844);
         markerDrag = googleMap.addMarker(new MarkerOptions()
                 .position(PosicionMover)
-                .title("Fija la ubicacion")
-                .draggable(true));
+                .title("Manten presionado para fijar la ubicacion!")
+                .draggable(true)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
         //Marker para arrastrar
         LatLng CineColombiaInfo = new LatLng(4.609416, -74.069080);
         InfoWindow = googleMap.addMarker(new MarkerOptions()
                 .position(CineColombiaInfo)
                 .title("Paqueadero CineColombia")
+                .snippet("Ver mas!!!")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
 
@@ -90,12 +96,35 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnInfoW
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        if (marker.equals(markerPrueba)){
-            String latitud,longitud;
-            latitud = Double.toString(marker.getPosition().latitude);
-            longitud = Double.toString(marker.getPosition().longitude);
-            Toast.makeText(this,"La latitud y longitud son:" + latitud + "," + longitud,Toast.LENGTH_SHORT).show();
-        }
+//        if (marker.equals(InfoWindow)){
+//            String latitud,longitud;
+//            latitud = Double.toString(marker.getPosition().latitude);
+//            longitud = Double.toString(marker.getPosition().longitude);
+//            Toast.makeText(this,"La latitud y longitud son:" + latitud + "," + longitud,Toast.LENGTH_SHORT).show();
+//        }
+//        if (marker.equals(markerPrueba)){
+//            String latitud,longitud;
+//            latitud = Double.toString(marker.getPosition().latitude);
+//            longitud = Double.toString(marker.getPosition().longitude);
+//            Toast.makeText(this,"La latitud y longitud son:" + latitud + "," + longitud,Toast.LENGTH_SHORT).show();
+//        }
+//        if (marker.equals(markerTadeo)){
+//            String latitud,longitud;
+//            latitud = Double.toString(marker.getPosition().latitude);
+//            longitud = Double.toString(marker.getPosition().longitude);
+//            Toast.makeText(this,"La latitud y longitud son:" + latitud + "," + longitud,Toast.LENGTH_SHORT).show();
+//        }
+//        if (marker.equals(markerLaTadeo)){
+//            String latitud,longitud;
+//            latitud = Double.toString(marker.getPosition().latitude);
+//            longitud = Double.toString(marker.getPosition().longitude);
+//            Toast.makeText(this,"La latitud y longitud son:" + latitud + "," + longitud,Toast.LENGTH_SHORT).show();
+//        }
+
+        String latitud,longitud;
+        latitud = Double.toString(marker.getPosition().latitude);
+        longitud = Double.toString(marker.getPosition().longitude);
+        Toast.makeText(this,"La latitud y longitud son:" + latitud + "," + longitud,Toast.LENGTH_SHORT).show();
         return false;
     }
 
@@ -134,6 +163,21 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnInfoW
         if (marker.equals(InfoWindow)){
             PopupMaker.newInstance(marker.getTitle(),
                     getString(R.string.InformacionMostrat))
+                    .show(getSupportFragmentManager(),null);
+        }
+        if (marker.equals(markerPrueba)){
+            PopupMaker.newInstance(marker.getTitle(),
+                    getString(R.string.InformacionMostratBiblioteca))
+                    .show(getSupportFragmentManager(),null);
+        }
+        if (marker.equals(markerTadeo)){
+            PopupMaker.newInstance(marker.getTitle(),
+                    getString(R.string.InformacionMostratUtadeo))
+                    .show(getSupportFragmentManager(),null);
+        }
+        if (marker.equals(markerLaTadeo)){
+            PopupMaker.newInstance(marker.getTitle(),
+                    getString(R.string.InformacionMostratLaTedo))
                     .show(getSupportFragmentManager(),null);
         }
     }
